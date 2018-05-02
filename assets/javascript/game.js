ef8c960a9bde95223921d6a1762f2787f0592da8
Raymond <rs01247@gmail.com>
2018-05-01 18:21:21 -0700

    started js file and added elements to index

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// INITIALIZATION
+var wins = 0;
+var userGuess = 0;
+var choose = "";
+
+// HTML ELEMENT NODES
+var winSpan = document.getElementById("wins");
+var userSpan = document.getElementById("userGuess");
+var chooseSpan = document.getElementById("choose");
