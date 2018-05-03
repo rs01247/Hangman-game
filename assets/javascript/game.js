80d1d2d647f9b0a393a8b0ef7b9039ac7f38fcbe
Raymond <rs01247@gmail.com>
2018-05-02 18:28:09 -0700

    edits in bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,16 @@ var choose = "";
 var winSpan = document.getElementById("wins");
 var userSpan = document.getElementById("userGuess");
 var chooseSpan = document.getElementById("choose");
+var avengers = ["Thanos", "Hulk", "Thor", "Hawkeye", "Loki"]
+var selction = avengers[Math.floor(Math.random() * avengers.length)];
+
+
+document.onkeyup = function(event) {
+    var userKey = event.key
+
+    if userKey === {
+
+    }
+
+
+}
