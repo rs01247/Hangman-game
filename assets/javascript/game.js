71ef0d75a695f77b2c4e11b6bd190bc7138fd411
Raymond <rs01247@gmail.com>
2018-05-02 22:30:58 -0700

    javascript started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,31 @@ var choose = "";
 var winSpan = document.getElementById("wins");
 var userSpan = document.getElementById("userGuess");
 var chooseSpan = document.getElementById("choose");
-var avengers = ["Thanos", "Hulk", "Thor", "Hawkeye", "Loki"]
-var selction = avengers[Math.floor(Math.random() * avengers.length)];
 
+// WORD CHOICES FOR THE GAME
+var avengers = [
+    "thanos", 
+    "hulk", 
+    "thor", 
+    "hawkeye", 
+    "loki"];
 
-document.onkeyup = function(event) {
-    var userKey = event.key
+// PICKS A RANDOM WORD    
+var selection = avengers[Math.floor(Math.random() * avengers.length)];
 
-    if userKey === {
-
-    }
-
-
+// CREATES THE BLANKS FOR WORD GUESS ACCORDING TO LENGTH OF WORD
+var answers = [];
+for (var i = 0; i < selection.length; i++) {
+    answers[i] = "_";
 }
+
+var remaining = selection.length;
+
+// WHILE LOOP THAT CONTINUES UNTIL THERE ARE NO MORE REMAINING LETTERS
+
+while (remaining > 0) {
+    var targetDiv = document.getElementById("startDiv");
+    targetDiv.textContent = "Hello";
+}
+
+answers.join(" ");
