d284a3fbe6bd5c73ed94c8b9e722476d8d869c86
Raymond <rs01247@gmail.com>
2018-05-04 22:18:04 -0700

    added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,102 @@
 
 window.onload = function () {
 
-    // INITIALIZATION   
+    // WORD CHOICES FOR THE GAME
+    var avengers = [
+        "hulk",
+        "thor",
+        "hawkeye",
+        "loki",
+        "antman",
+        "spiderman",
+        "ironman",
+        "blackpanther",
+    ];
+
+    var images = [
+        "../images/hulk.jpeg",
+        "../images/thor.png",
+        "../images/hawkeye.png",
+        "../images/loki.png",
+        "../images/antman.png",
+        "../images/spiderman.png",
+        "../images/ironman.png",
+        "../images/blackpanther.png",
+    ];
 
     var wins = 0;
     var userGuess = remaining;
     var letters = [];
 
+    // INITIALIZATION   
+    var selection = avengers[Math.floor(Math.random() * avengers.length)];
+    console.log(selection);
     // HTML ELEMENT NODES
     var winSpan = document.getElementById("wins");
     var userSpan = document.getElementById("userGuess");
     var letterSpan = document.getElementById("letters");
 
-    // WORD CHOICES FOR THE GAME
-    var avengers = [
-        "thanos",
-        "hulk",
-        "thor",
-        "hawkeye",
-        "loki"
-    ];
-
-    var selection = avengers[Math.floor(Math.random() * avengers.length)];
+    // ANSWERS ARRAY THAT IS DISPLAYED
     var answers = [];
     for (var i = 0; i < selection.length; i++) {
         answers[i] = "_";
     }
-
     var remaining = selection.length;
+    console.log(remaining)
+
+
+    // CREATING FUNCTIONS FOR RESTARTING THE GAME AND UPDATING INFORMATION
+    function restartGame() {
+        selection = avengers[Math.floor(Math.random() * avengers.length)];
+        userGuess = remaining;
+        letters = [];
+        updateHTML();
+    }
+
+    function updateHTML() {
+        winSpan.textContent = wins;
+        userSpan.textContent = userGuess;
+        letterSpan.textContent = letters;
+    }
 
     // GAME LOOP    
-    // while (remaining > 0) {
+
+    //JS TO MAKE GAME START WITH BUTTON PRESS
+    // document.getElementById("startDiv").addEventListener("click", function (event) {
 
     document.onkeydown = function (event) {
-        var targetDiv = document.getElementById("startDiv");
-        targetDiv.innerHTML = answers.join(" ");
-        console.log(answers.join(" "));
-
-// CURRENT BUG - CORRECT LETTER SHOWS UP IN THE BLANKS WHEN THE SECOND LETTER IS PRESSED
-
         var input = event.key;
         for (var j = 0; j < selection.length; j++) {
             if (selection[j] === input) {
-            answers[j] = input;
-            remaining--;
+                answers[j] = input;
+                remaining--;
             }
-        }
-    }
-// }
 
-    winSpan.textContent = wins;
-    userSpan.textContent = userGuess;
-    letterSpan.textContent = letters;
+            // NEED TO LOG VAR REMAINING TO LETTERS REMAINING
+            // NEED TO LOG LETTERS USED
+
+            var targetDiv = document.getElementById("startDiv");
+            targetDiv.innerHTML = (answers.join(" ")).toUpperCase();
+            console.log(selection);
+
+            // WHEN CORRECT WORD IS GUESSED, DISPLAY DIV WITH CHARACTER PHOTO AND NAME
+            // NEED TO SET ID AND CHANGE IMG AND P FOR CARD
+            // IF STATEMENT THAT RESTARTS GAME WHEN COMPLETED
+            //LOG WIN TO "wins"
+        }
+        userGuess--;
+        letters.push(input);
+        if (remaining = 0) {
+            wins++;
+            restartGame();
+        }
+
+        else {
+            updateHTML();
+        }
+
+    }
 
 }
+
+
